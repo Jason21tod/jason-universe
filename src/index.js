7dabe33d188411c02d98b3e9b8dc521d0693003c
Gian_Pereira <gian.21.otaku@gmail.com>
2024-11-27 22:41:01 -0300

    Preparing to content

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
+import Main from './components/App.jsx';
+import reportWebVitals from './reportWebVitals.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Main />
   </React.StrictMode>
 );
 
