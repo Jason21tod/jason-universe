ea460808ba0d8c09927bb3424bf2afa4dfa7bc6c
Gian_Pereira <gian.21.otaku@gmail.com>
2024-12-05 23:54:31 -0300

    "doing new animations improvments and more minor changes"

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,29 +2,66 @@ import './reset.css';
 import './Main.css';
 import '../styles/Generics.css';
 import '../styles/Header_container.css';
+import './who_iam.css'
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 
 function Main() {
   return (
-      <main className='generic-container'>
-        <h2>Sobre Mim</h2>
-        <p className='generic-container--paragraph'>
-          Olá, me chamo Gian Pereira e tenho 22 anos.
-          Atúo no mercado de desenvolvimento desde os 20 anos com meu primeiro projeto para o Clara Resorts,
-          o qual, foi o unico maior projeto qual participei.
-        </p>
-        <p className='generic-container--paragraph'>
-          Desde então, tenho buscado projetos e experiência para entrar de vez na área de programação.
-        </p>
-        <p className='generic-container--paragraph'>
-          Meu principal foco em desenvolvimento de soluções de automação web, desenvolvimento de sites e melhorias
-          de processos empresariais.
-        </p>
-        <p className='generic-container--paragraph'>
-          Por quê Jason? um amigo meu certa vez me disse que é mais facil de pronunciar.
-        </p>
+      <main className="who-iam-container">
+        <WhoIam></WhoIam>
       </main>
   );
+}
+
+function ScrollAnimation({ children }) {
+  const [isVisible, setIsVisible] = useState(false);
+  const elementReference = useRef(null);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (!elementReference.current) return;
+
+      const rect = elementReference.current.getBoundingClientRect();
+      const isInViweport = rect.top <= window.innerHeight && rect.bottom >= 0;
+
+      if (isInViweport) {
+        setIsVisible(true);
+      } else {
+        setIsVisible(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <div
+      ref={elementReference}
+      className={isVisible ? "generic-container generic-container--visible":"generic-container generic-container--hidden"}
+      >
+      {children}
+    </div>
+  )
+}
+
+function WhoIam() {
+  return(
+    <ScrollAnimation >
+      <h2 className='generic-container--title'>Sobre Mim</h2>
+      <p className='generic-container--paragraph'>
+        Olá, me chamo Gian Pereira e tenho 22 anos.
+        Atúo no mercado de desenvolvimento desde os 20 anos com meu primeiro projeto para o Clara Resorts,
+        o qual, foi o unico maior projeto qual participei.
+      </p>
+      <p className='generic-container--paragraph'>
+        Meu principal foco em desenvolvimento de soluções de automação web, desenvolvimento de sites e melhorias
+        de processos empresariais.
+      </p>
+    </ScrollAnimation>
+  )
 }
 
 export default Main;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,27 +1,44 @@
 function Header() {
-  const title_text = "Vamos construir seu universo?";
+  const title_text = "Vamos";
+  const tittle_text_2 = " Construir";
+  const tittle_text_3 = " Seu";
+  const tittle_text_4 = " Universo?"
 
   return (
     <header className="header-container">
       <h1 className="header-container_title">
         {/* O codigo abaixo cria uma animação de pop up em letra por letra */}
-        {Array.from(title_text).map((char, index) => (
-          <span
-            key={index}
-            style={{
-              animation: "fadeIn 0.2s", //Hoje aprendi a criar uma animação :D
-              animationDelay: `${index * 0.1}s`,
-              display: "inline-block"
-            }}
-          >
-            {char === " " ? "\u00A0" : char}
-          </span>
-        ))}
+        <TitleAnimator word={title_text} anim_time={"0.2s"} delay_coefficient={0.1}></TitleAnimator>
+        <TitleAnimator word={tittle_text_2} anim_time={"0.2s"} delay_coefficient={0.1}></TitleAnimator>
+        <TitleAnimator word={tittle_text_3} anim_time={"0.2s"} delay_coefficient={0.1}></TitleAnimator>
+        <TitleAnimator word={tittle_text_4} anim_time={"0.2s"} delay_coefficient={0.1}></TitleAnimator>
       </h1>
-      <legend>Gian P. Nunes</legend>
+      <legend className="header-container_legend">Gian P. Nunes, mas pode me chamar de Jason</legend>
       <p>Desenvolvedor FullStack</p>
     </header>
   );
+}
+
+function TitleAnimator ({word, anim_time, delay_coefficient}) {
+   let anim_config = `${"fadeInY "+anim_time}`;
+  return (
+    <p>
+      {Array.from(word).map((char, index) => (
+        <span
+          key={index}
+          style={{
+            animation: anim_config, //Hoje aprendi a criar uma animação :D
+            animationDelay: `${index * delay_coefficient}s`,
+            display: "inline-block",
+            fontSize: "45px"
+          }}
+        >
+          {char === " " ? "\u00A0" : char}
+        </span>
+        )
+      )}
+    </p>
+  )
 }
 
 export default Header;
