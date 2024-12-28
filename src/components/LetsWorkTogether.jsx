b713496179ca9458f30a75a156c86bf374e57291
Gian_Pereira <gian.21.otaku@gmail.com>
2024-12-28 14:06:21 -0300

    finishing some CSS styles

--- src/components/LetsWorkTogether.jsx
+++ src/components/LetsWorkTogether.jsx
@@ -5,12 +5,18 @@ function LetsWorkTogether () {
         <section className="lets_work-section">
             <h2 className="lets_work-title">Lets Work Together</h2>
             <legend className="lets_work-legend">Im glad that you stayed, so let's build your universe?</legend>
-            <form className="lets_work-form">
-                <label htmlFor="name">Name: <input type="text" name="name" id="name" /></label>
-                <label htmlFor="email">Email: <input type="email" name="email" id="email" /></label>
-                <label htmlFor="proposal">Proposal: <input type="text" name="proposal" id="email"/></label>
-                <button type="submit">Lets Go!</button>
-            </form>
+            <div className='lets_work-form_area'>
+                <img src="" alt="universe" className="lets_work-img"/>
+                <form className="lets_work-form">
+                    <label htmlFor="name">Name: </label>
+                    <input type="text" name="name" id="name" />
+                    <label htmlFor="email">Email: </label>
+                    <input type="email" name="email" id="email" />
+                    <label htmlFor="proposal">Proposal: </label> 
+                    <input type="text" name="proposal" id="email"/>
+                    <button type="submit">Lets Go!</button>
+                </form>
+            </div>
         </section>
     )
 }
