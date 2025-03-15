14403e8f33a333cdb42df9507d4a2beba864508e
Gian_Pereira <gian.21.otaku@gmail.com>
2025-03-15 09:47:58 -0300

    Preparing the image from about me Section

--- src/components/sections/who_iam/WhoIam.jsx
+++ src/components/sections/who_iam/WhoIam.jsx
@@ -5,22 +5,27 @@ import ScrollAnimation from '../../utils/text_animations.jsx'
 function WhoIam() {
   return(
     <section id='about-section' className='who-iam-container'>
-      <ScrollAnimation >
-        <h2 className='scroll-animation--title'>Sobre Mim</h2>
-        <p className='scroll-animation--paragraph'>
-          Olá, me chamo Gian Pereira e tenho 22 anos. Atuamente, trabalho como freelancer.
-          Meu principal foco é desenvolvimento de soluções de automação, desenvolvimento de sites e melhorias
-          de processos empresariais e relacionados ao cliente.
-        </p>
-      </ScrollAnimation>
-      <ScrollAnimation>
-        <p className='scroll-animation--paragraph'>
-          Desenvolvo/desenvolvi soluções de chat; Pedidos Online; Sites simples; automações de escala de trabalho; Hotelaria e mais;
-        </p>
-      </ScrollAnimation>
-      <ScrollAnimation>
-        <img id='planet' src="./images/astra-planet-64x64.png" alt="collection of projects" />
-      </ScrollAnimation>
+      <div className='who-iam--column'>
+        <ScrollAnimation >
+          <h2 className='who-iam--title'>Sobre Mim</h2>
+          <p className='who-iam--paragraph'>
+            Olá, me chamo Gian Pereira e tenho 22 anos. Atualmente, trabalho como freelancer para renda extra.
+            Meu principal foco é desenvolvimento de soluções de automação, desenvolvimento de sites e melhorias
+            de processos empresariais e relacionados ao cliente.
+          </p>
+        </ScrollAnimation>
+        <ScrollAnimation>
+          <p className='who-iam--paragraph'>
+            Meu maior trabalho foi feito para a rede de hotéis Clara Resorts, durante meu período lá, entretanto,
+            já desenvolvi projetos menores, tanto voluntáriados quanto projetos remunerados.
+          </p>
+        </ScrollAnimation>
+      </div>
+      <div className='who-iam--column'>
+        <ScrollAnimation>
+          <img id='planet' src="./images/astra-planet-64x64.png" alt="collection of projects" />
+        </ScrollAnimation>
+      </div>
     </section>
   )
 }
