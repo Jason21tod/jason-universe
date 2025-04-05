98a84011dd83c2967a6fdca61d5bcb390d47f847
Gian_Pereira <gian.21.otaku@gmail.com>
2025-04-05 07:34:27 -0300

    making more icons

--- src/components/sections/footer/Footer.jsx
+++ src/components/sections/footer/Footer.jsx
@@ -4,6 +4,8 @@ import './Footer.css';
 function Footer () {
     return (
         <section className='footer-section'>
+            <div className='footer-section--links'>
+            </div>
             <legend className='footer-legend'>All Rights Reserved</legend>
             <p className='footer-text'>Gian Pereira Nunes</p>
         </section>
