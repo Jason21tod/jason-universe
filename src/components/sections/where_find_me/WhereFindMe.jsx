ed204e10e83e3d7823c747f7700a3837ccfaf42f
Gian_Pereira <gian.21.otaku@gmail.com>
2025-04-06 07:24:56 -0300

    fixing the links

--- src/components/sections/where_find_me/WhereFindMe.jsx
+++ src/components/sections/where_find_me/WhereFindMe.jsx
@@ -8,8 +8,8 @@ function WhereFindMe () {
             <div className="findme--link_block">
                 <a id='github' href="https://github.com/Jason21tod"></a>
                 <a id='linkedin' href="https://www.linkedin.com/in/gian-pereira-595b48205"></a>
-                <a id='upwork' href="https://www.fiverr.com/jason_universe"></a>
-                <a id='fiverr' href="https://www.upwork.com/freelancers/~01c85774bb5d8274f5?mp_source=share"></a>
+                <a id='upwork' href="https://www.upwork.com/freelancers/~01c85774bb5d8274f5?mp_source=share"></a>
+                <a id='fiverr' href="https://www.fiverr.com/jason_universe"></a>
             </div>
         </section>
     )
